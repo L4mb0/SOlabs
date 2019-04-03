c77b51dbc60b52840a037d83392c2c1060dfc703
F0rn1x <juan.navarro@utec.edu.pe>
2019-04-02 20:13:05 -0500

    Update info_pc2_juannavarro.c

--- info_pc2_juannavarro.c
+++ info_pc2_juannavarro.c
@@ -8,14 +8,38 @@ int sleep();
 
 int main () {
     char command[50];
-
+    bool counter=true;
 
     while(true) {
-
-        strcpy(command, "free | grep Memoria | awk '{print $4/$2 * 100.0}'");
+        //memoria disponible
+        strcpy(command, "free | grep Mem | awk '{print $4/$2 * 100.0}'");
         system(command);
 
-    sleep(1);
+        ///////////////////////////////////////////////////////////
+
+        //tiempo en user
+        strcpy(command, "cat /proc/stat | grep -w 'cpu' | awk '{print ($2/($2+$3+$4+$5+$6+$7+$8)) * 100.0}'");
+        system(command);
+
+        //tiempo en system
+        strcpy(command, "cat /proc/stat | grep -w 'cpu' | awk '{print ($4/($2+$3+$4+$5+$6+$7+$8)) * 100.0}'");
+        system(command);
+
+        //tiempo en idle
+        strcpy(command, "cat /proc/stat | grep -w 'cpu' | awk '{print ($5/($2+$3+$4+$5+$6+$7+$8)) * 100.0}'");
+        system(command);
+
+        ///////////////////////////////////////////////////////////
+
+        //tiempo en idle
+        if (counter == true) {
+            strcpy(command, "cat /proc/stat | grep -w 'ctxt' | awk '{print $2}'");
+            long x = system(command);
+        }
+
+
+        break;
+        sleep(1);
 
     }
 
