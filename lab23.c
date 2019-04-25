861a4769bb8273d92ab359555a4dfa166792b2ac
F0rn1x <juan.navarro@utec.edu.pe>
2019-04-24 21:36:51 -0500

    Update lab23.c

--- lab23.c
+++ lab23.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sched.h>
 #include <time.h>
 #include <sys/types.h>
 #include <signal.h>
@@ -10,101 +11,101 @@
 #include <sys/wait.h>
 #include <pthread.h>
 #include <semaphore.h>
-
 #define INDEX 5
-
-
 /////////////////////////////////////////
-
 int filosofos[INDEX] = {0, 1, 2, 3, 4};
-int palitosFilosofos[INDEX]={0,0,0,0,0};
-int palitos = 5;
-int makis[INDEX] = {20, 20, 20, 20, 20};
-int sum = 0;
+int palitos[INDEX] = {1, 1, 1, 1, 1};
+bool c =true;
+int procesosTerminados = 0;
+int filoVivos=5;
+int llegoHilo=0;
 int tiempo = 0;
 int segundos, minutos, horas, segundosRestantes;
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-pthread_barrier_t barrier;
-
+static pthread_barrier_t barrera;
 /////////////////////////////////////////
-
 void *comer(void *idFilosofo) {
+
+    int *id = (int *) idFilosofo;
+    int misPalitos = 0;
+    int misMakis = 20;
     while (true) {
-        int *id = (int *) idFilosofo;
         pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&cond, &mutex);
+        llegoHilo++;
+        if(llegoHilo==5){
+            llegoHilo=0;
+            pthread_cond_broadcast(&cond);
+        }
+        else
+            pthread_cond_wait(&cond, &mutex);
         pthread_mutex_unlock(&mutex);
-        //pthread_barrier_wait(&barrier);
-
-
-        if (palitos > 1) {
-                palitos--;
-                palitosFilosofos[*id]++;
-                if(palitos>1) {
-                    palitos--;
-                    palitosFilosofos[*id]++;
-                    if (palitosFilosofos[*id] == 2) {
-                        makis[*id]--;
-                        printf("Filosofo %d esta comiendo\n", *id);
-                    }
+        misPalitos=0;
+        pthread_mutex_lock(&mutex);
+        c=true;
+        procesosTerminados=0;
+        pthread_mutex_unlock(&mutex);
+        for (int i = 0; i < INDEX; ++i) {
+            if (misPalitos < 2) {
+                pthread_mutex_lock(&mutex);
+                if (palitos[i] == 1) {
+                    palitos[i] = 0;
+                    misPalitos++;
                 }
+                pthread_mutex_unlock(&mutex);
+            }
+            if (misPalitos == 2)break;
+        }
+
+        if (misPalitos == 2) {
+            misMakis--;
+            printf("Filosofo %d esta comiendo\n", *id);
+            pthread_mutex_lock(&mutex);
+            if(c ==true){
+                c=false;
+                tiempo+=4;
+            };
+            pthread_mutex_unlock(&mutex);
         }
         else {
             printf("Filosofo %d esta hablando\n", *id);
         }
 
-        if (makis[*id] == 0)break;
+        pthread_mutex_lock(&mutex);
+        procesosTerminados++;
+        if(procesosTerminados == filoVivos){
+            for (int i = 0; i < INDEX; ++i) {
+                palitos[i]=1;
+            }
+            printf("//\n");
+        }
+        pthread_mutex_unlock(&mutex);
+        if (misMakis == 0)break;
     }
+
+    printf("mate el thread %d\n\n", *id);
+    pthread_mutex_lock(&mutex);
+    filoVivos--;
+    pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
 }
 
 
 int main() {
 
-    pthread_barrier_init(&barrier, NULL, INDEX);
     pthread_t tid[INDEX];
-
-
+    pthread_barrier_init(&barrera, NULL, INDEX);
     for (int i = 0; i < INDEX; i++)
         pthread_create(&tid[i], NULL, comer, &filosofos[i]);
-
     sleep(1);//espera para que todos lleguen al lock
-    while(true) {
-        pthread_cond_broadcast(&cond); //GO
-
-        sched_yield();
-
-        palitos=5;
-        for (int j = 0; j < 5; ++j) {
-            palitosFilosofos[j]=0;
-        }
-        printf("\n");
-        for (int i = 0; i < 5; ++i) {
-            sum += makis[i];
-        }
-        if (sum==0)break;
-        sum = 0;
-        tiempo += 4;
-    }
-
-
-
     for (int i = 0; i < INDEX; i++)
         pthread_join(tid[i], NULL);
-
-    //pthread_barrier_destroy(&barrier);
-
-
-    horas=tiempo/(60*60);
-    segundosRestantes = tiempo-(horas*60*60);
-    minutos=segundosRestantes/60;
-    segundosRestantes=segundosRestantes-(minutos*60);
-    segundos=segundosRestantes;
-
+    horas = tiempo / (60 * 60);
+    segundosRestantes = tiempo - (horas * 60 * 60);
+    minutos = segundosRestantes / 60;
+    segundosRestantes = segundosRestantes - (minutos * 60);
+    segundos = segundosRestantes;
     printf("%d segundos\n", tiempo);
     printf("%d horas // %d minutos // %d segundos", horas, minutos, segundos);
-
     return 0;
 }
